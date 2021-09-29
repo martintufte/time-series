e7076ccfef5753f047633e21a3031dfe15a79125
Martin Gudahl Tufte <martingudahltufter@hotmail.com>
2021-09-29 15:23:39 +0200

    Update read_data.R

--- read_data.R
+++ read_data.R
@@ -1,3 +1,14 @@
-# file to read data
+# Remember to set the working directory
+# Session -> Set Working Directory -> To Source File Location
 
-df <- read.csv(file = 'data/CBBTCUSD.csv', stringsAsFactors = TRUE)
+
+# file to read data
+df <- read.csv(file = "data/CBBTCUSD.csv")
+
+# number of observations
+n <- dim(df)[1]
+
+# plot the initial data
+plot(1:n, df$CBBTCUSD, type='l')
+
+
