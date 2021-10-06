b78a33ec93bdeba8131693f52f64de6769f781e3
Martin Gudahl Tufte <martingudahltufter@hotmail.com>
2021-10-06 11:50:08 +0200

    Update sacf.R

--- functions/sacf.R
+++ functions/sacf.R
@@ -8,7 +8,7 @@ sacf <- function(x, max.lag=12){
   for (h in 0:min(max.lag, n-1)){
     gamma.hat[h+1] <- 0
     for (t in 1:(n-h)){
-      gamma.hat[h+1] <- gamma.hat[h+1] + (x[t] - x.bar)*(x[t+h] - x.bar)
+      gamma.hat[h+1] <- gamma.hat[h+1] + (x[t+h] - x.bar)*(x[t] - x.bar)
     }
   }
   
@@ -16,7 +16,5 @@ sacf <- function(x, max.lag=12){
   rho.hat <- gamma.hat / gamma.hat[1]
   
   output <- list( gamma.hat = gamma.hat,
-                  rho.hat = rho.hat,
-                  lags = 0:max.lag)
-  return(output)
+                  rho.hat = rho.hat)
 }
