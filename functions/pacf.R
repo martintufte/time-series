e5f3f767a256f9210b2ebbbd8d5cf5552488a8fd
Martin Gudahl Tufte <martingudahltufter@hotmail.com>
2021-10-06 11:54:50 +0200

    Small fix

--- functions/pacf.R
+++ functions/pacf.R
@@ -6,11 +6,9 @@ pacf <- function(x,max.lag = 12){
   phi.hat <- numeric(max.lag+1)
   sacf <- sacf(x)
   
-  for(h in 1:min(max.lag,n-1))
-  {
+  for (h in 1:min(max.lag,n-1)){
     vec <- solve(toeplitz(sacf$rho.hat[1:h]), sacf$rho.hat[2:(h+1)])
     phi.hat[h] <- vec[length(vec)]
-    
   }
   output <- list(phi.hat = phi.hat)
   return(output)
