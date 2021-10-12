e39b1c979ff2079c4c6a1b25de87f738ceaf1278
Martin Gudahl Tufte <martingudahltufter@hotmail.com>
2021-10-12 14:30:42 +0200

    Fix prepping, structurated plots, fix bug in pacf-function

--- read_data.R
+++ read_data.R
@@ -1,51 +1,64 @@
 # Remember to set the working directory
 # Session -> Set Working Directory -> To Source File Location
 
+source('functions/sacf.R')
+source('functions/pacf.R')
+source('functions/DurbinLevinson.R')
+library(stats)
 
-# file to read data
+
+
+  ### DATA PREP ###
+
 df <- read.csv(file = "data/CBBTCUSD.csv")
 
+# remove earlier dates than 2015-01-19 (as they contain lots of NA)
+df <- df[as.Date(df$DATE) > as.Date("2015-01-19"),]
+# fix NAN at "2020-09-04"
+df[df$DATE=="2020-09-04",'CBBTCUSD'] = 10493.77
 # convert to numeric values
 df$CBBTCUSD <- as.numeric(df$CBBTCUSD)
-
 # number of observations
 n <- dim(df)[1]
 
 
-# NA handling
-idx.na <- which( is.na(df$CBBTCUSD) )
-for (i in idx.na){
-  if (i == 0){
-    df$CBBTCUSD[i] = df$CBBTCUSD[i+1]
-  }
-  else if (i == n){
-    df$CBBTCUSD[i] = df$CBBTCUSD[i-1]
-  }
-  else {
-    df$CBBTCUSD[i] = 1/2 * (df$CBBTCUSD[i-1] + df$CBBTCUSD[i+1])
-  }
-}
 
-
-# create time-series object
-library(stats)
-ts <- as.ts(df$CBBTCUSD)
-
-# log of time series
-ts.log <- log(ts)
-
-# Import sample autocovariance function
-source('functions/sacf.R')
+  ### PLOT DATA ###
 
 par(mfrow=c(2,2),mar=c(5,4,1,2))
 
+ts <- as.ts(df$CBBTCUSD)
+ts.log <- log(ts)
+ts.diff <- diff(ts)
+ts.log.diff <- diff(ts.log)
+
+
+# ts
 plot(ts, ylab='USD')
 plot(ts.log, ylab='log(USD)')
-plot(sacf(ts, max.lag=100)$rho.hat, type='o', xlab='lag',ylab='sacf')
-plot(sacf(ts.log, max.lag=100)$rho.hat, type='o', xlab='lag',ylab='sacf of log')
+plot(ts.diff, ylab='differentiated USD')
+plot(ts.log.diff, ylab='differentiated log(USD)')
+
+# sacf
+plot(sacf(ts, max.lag=20)$rho.hat, type='o', xlab='lag',ylab='sacf')
+plot(sacf(ts.log, max.lag=20)$rho.hat, type='o', xlab='lag',ylab='sacf of log')
+plot(sacf(ts.diff, max.lag=20)$rho.hat, type='o', xlab='lag',ylab='sacf of diff')
+plot(sacf(ts.log.diff, max.lag=20)$rho.hat, type='o', xlab='lag',ylab='sacf of diff of log')
+
+# pacf
+plot(pacf(ts, max.lag=20)$phi.hat, type='o', xlab='lag',ylab='pacf')
+plot(pacf(ts.log, max.lag=20)$phi.hat, type='o', xlab='lag',ylab='pacf of log')
+plot(pacf(ts.diff, max.lag=20)$phi.hat, type='o', xlab='lag',ylab='pacf of diff')
+abline(h=1.96/sqrt(n), col='red')
+abline(h=-1.96/sqrt(n), col='red')
+plot(pacf(ts.log.diff, max.lag=20)$phi.hat, type='o', xlab='lag',ylab='pacf of diff of log')
+abline(h=1.96/sqrt(n), col='red')
+abline(h=-1.96/sqrt(n), col='red')
 
 
-p = 2
-q = 2
-fit <- arima(ts.log, order = c(p, 0, q))
+### estimate ARIMA(p,d,q) ###
+
+
+
+### Forecasting ###
 
