9353b3006d4435e595cef29cdc52dc2b01421490
Martin Gudahl Tufte <martingudahltufter@hotmail.com>
2021-10-20 16:57:07 +0200

    Fix plot of LSTM.

--- LSTM.py
+++ LSTM.py
@@ -37,79 +37,56 @@ df.loc[:,'logBTC'] = np.log(df.BTC)
 ### remove trend component, assume linear trend m(t) = beta0 + beta1 * t
 
 # design matrix and response (y = X beta + eps)
-X_matr = np.array([np.ones(df.shape[0]), np.sqrt(df.index)]).T
+X_matr = np.array([np.ones(df.shape[0]), df.index]).T
 y_vec = np.array(df.logBTC)
 # MLE estimate for beta (beta = (X^T X)^(-1) X^T y)
 beta0, beta1 = la.solve(X_matr.T @ X_matr, X_matr.T @ y_vec)
 # trend component
-df.loc[:,'m'] = beta0 + beta1 * np.sqrt(df.index)
+df.loc[:,'m'] = beta0 + beta1 * df.index
 def trend(x):
-  return beta0 + beta1 * np.sqrt(x)
-
-
-### plot the data with trend component
-plt.plot(df.logBTC)
-plt.plot(df.m)
-plt.show()
-
+  return beta0 + beta1 * x
 df.loc[:,'Y'] = df.logBTC - df.m
 
 
-### scale data and reshape to use in LSTM?
 
-#scaler = MinMaxScaler(feature_range = (-1, 1)).fit(train.CBBTCUSD)
-#train.CBBTCUSD = scaler.transform(train.CBBTCUSD)
-#test.CBBTCUSD = scaler.transform(test.CBBTCUSD)
+### plot the data with trend component
+
+#plt.plot(df.logBTC)
+#plt.plot(df.m)
+#plt.show()
 
 
 
+### split into training and test data set (use last 365 days for test)
 
-### define Long-Short Term model
+#split_idx = np.shape(df)[0] - 365
+#df_train = df[:split_idx]
+#df_test = df[split_idx:]
 
-# split a univariate sequence
-def split_sequence(sequence, n_steps):
-	X, y = list(), list()
-	for i in range(len(sequence)):
-		# find the end of this pattern
-		end_idx = i + n_steps
-		# check if we are beyond the sequence
-		if end_idx > len(sequence)-1:
-			break
-		# gather input and output parts of the pattern
-		seq_x, seq_y = sequence[i:end_idx], sequence[end_idx]
-		X.append(seq_x)
-		y.append(seq_y)
-	return np.array(X), np.array(y)
+
+
+### create Long Short-Term model
+def split_sequence(seq, n_steps):
+    X = []
+    y = []
+    for i in range(len(seq)):
+        # find the end of this pattern
+        end_idx = i + n_steps
+        # check if we are beyond the sequence
+        if end_idx > len(seq)-1:
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = seq[i:end_idx], seq[end_idx]
+        X.append(seq_x)
+        y.append(seq_y)
+	
+    return np.array(X), np.array(y)
  
-# define input sequence
-raw_seq = np.array(df.Y)
-
-# choose a number of time steps
-n_steps = 100
-
-# split into samples
-X, y = split_sequence(raw_seq, n_steps)
-
-# reshape from [samples, timesteps] into [samples, timesteps, features]
-n_features = 1
-X = X.reshape((X.shape[0], X.shape[1], n_features))
-
-# define model
-model = Sequential()
-model.add(LSTM(20, activation='relu', input_shape=(n_steps, n_features)))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
-
-# fit model
-model.fit(X, y, epochs=20, verbose=0)
-model.summary()
-
-plt.plot(df.index, df.logBTC - df.logTrend)
-plt.plot(df.index[n_steps:], model.predict(X))
-plt.show()
-
 
 def predict_lstm_hstep(model, seq, h=1):
+    '''
+    Predicting h steps into the future
+    '''
     values = np.zeros(n_steps + h)
     values[:n_steps] = seq[-n_steps:] # set first n_steps elements
     for i in range(h):
@@ -121,27 +98,67 @@ def predict_lstm_hstep(model, seq, h=1):
     return prediction
 
 
-### plot prediction
-h = 365
 
-prediction = predict_lstm_hstep(model, raw_seq, h)
-p_index = range(df.index[-1]+1, df.index[-1]+1+h)
 
-plt.plot(df.index, df.logBTC)
-plt.plot(p_index, trend(p_index) + prediction)
+# Arcitecture
+n_steps    = 100              # choose a number of inputs nodes
+n_lstm     = 20               # choose the number of LSTM nodes
+n_features = 1                # choose number of outputs nodes
+n_epochs   = 20               # number of training epochs
+h          = 365              # forecast one year forwards
+n_models   = 5                # number og models fitted
+
+# create training data
+raw_seq = np.array(df.Y)
+# define input time series; split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# reshape from [samples, timesteps] into [samples, timesteps, features]
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+models = []
+predictions = []
+
+for i in range(5):
+    # define model
+    model = Sequential()
+    model.add(LSTM(n_lstm, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    
+    # fit model
+    model.fit(X, y, epochs=n_epochs, verbose=0)
+    model.summary()
+    
+    # predict one year forward
+    prediction = predict_lstm_hstep(model, raw_seq, h)
+    
+    # add to list
+    models.append(model)
+    predictions.append(prediction)
+
+
+
+plt.figure(figsize=(8, 6), dpi=80)
+
+# plot prediction with the training input
+plt.plot(df.index - 2158, df.logBTC - df.m, color='black', linewidth=1)
+for model in models:
+    plt.plot(df.index[n_steps:] - 2158, model.predict(X), color='r--', linewidth=1)
+plt.xlabel('t')
+plt.ylabel('Y')
+plt.savefig('LSTM_Ys.pdf')
+plt.show()
+
+pred_plot_index = range(df.index[-1]+1 - 2158, df.index[-1]+1+h - 2158)
+pred_index = range(df.index[-1]+1, df.index[-1]+1+h)
+
+plt.plot(df.index - 2158, df.logBTC, color='black', linewidth=1)
+for prediction in predictions:
+    plt.plot(pred_plot_index, trend(pred_index) + prediction, color='red', linewidth=1)
+plt.plot()
+plt.xlabel('t')
+plt.ylabel('log(USD)')
+plt.savefig('LSTM_predictions.pdf')
 plt.show()
 
 
-h = 1000
-
-prediction = predict_lstm_hstep(model, raw_seq, h)
-p_index = range(df.index[-1]+1, df.index[-1]+1+h)
-
-plt.plot(df.index, df.logBTC)
-plt.plot(p_index, trend(p_index) + prediction)
-plt.show()
-
-
-
-
-#'''
