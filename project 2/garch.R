d8c5b3cdbf56922f7dad86218245535f708a7ef0
Martin Gudahl Tufte <martingudahltufter@hotmail.com>
2021-11-24 18:14:01 +0100

    Update Garch.

--- project 2/garch.R
+++ project 2/garch.R
@@ -2,7 +2,7 @@ library(rugarch)
 library(forecast)
 
 ## function for finding the best model, then plotting it
-plotgarch <- function(data,arma.order,garch.model,i.max = 2, j.max = 2, h = 365) {
+plotgarch <- function(data, arma.order, garch.model, i.max = 2, j.max = 2, h = 365) {
   
   criteria <- list()
   for(i in 1:i.max){
@@ -20,16 +20,18 @@ plotgarch <- function(data,arma.order,garch.model,i.max = 2, j.max = 2, h = 365)
   j.aic = model.criteria[which.min(model.criteria$aic),]$j
   
   model.spec <- ugarchspec(variance.model = list(model=garch.model,garchOrder=c(i.aic,j.aic)),mean.model = list(armaOrder=arma.order,include.mean=FALSE),distribution.model = "std")
-  model=ugarchfit(spec=model.spec, data=data)
+  model = ugarchfit(spec=model.spec, data=data)
   
   n <- length(data)
   
   f.model <- ugarchforecast(model,n.ahead=h)
-  plot(1:n,Z[1:n],type="l",xlim=c(0,(n+h)))
+  plot(Z,type="l",xlim=c(0,(n+h)),ylab=as.character(garch.model))
   lines(seq(n+1,n+h),f.model@forecast$seriesFor,col="red")
   lines(seq(n+1,n+h),f.model@forecast$seriesFor-qt(0.025,9)*f.model@forecast$sigmaFor,col="blue")
   lines(seq(n+1,n+h),f.model@forecast$seriesFor+qt(0.025,9)*f.model@forecast$sigmaFor,col="blue")
   
+  # return model
+  model
 }
 
 par(mfrow=c(2,2))
@@ -42,18 +44,26 @@ h <- 365 # predict 1 year ahead
 
 # GARCH
 garch.model = "sGARCH"
-plotgarch(data,arma.order,garch.model,2,2,h)
+mod.GARCH <- plotgarch(data,arma.order,garch.model,5,5,h)
 
 # eGARCH
 garch.model = "eGARCH"
-plotgarch(data,arma.order,garch.model,2,2,h)
+mod.eGARCH <- plotgarch(data,arma.order,garch.model,5,5,h)
 
 # iGARCH
 garch.model = "iGARCH"
-plotgarch(data,arma.order,garch.model,2,2,h)
+mod.iGARCH <- plotgarch(data,arma.order,garch.model,5,5,h)
 
 # fiGARCH
 garch.model = "fiGARCH"
-plotgarch(data,arma.order,garch.model,2,2,h)
+mod.fiGARCH <- plotgarch(data,arma.order,garch.model,3,3,h)
+
+
+### Extracting parameters
+mod.GARCH
+mod.eGARCH
+mod.iGARCH
+mod.fiGARCH
+
 
 
